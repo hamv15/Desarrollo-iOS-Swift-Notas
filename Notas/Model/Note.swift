19bf2bef62ca1eeebcd699a51407464ddee36a25
Hugo Adrián Meza Vega <hamv15@hotmail.com>
2024-04-19 19:22:14 -0600

    Initial Commit

---
+++ Notas/Model/Note.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Notas
+//
+//  Created by Hugo Adrián Meza Vega on 19/04/24.
+//
+
+import Foundation
+
+
+struct Note: Codable {
+    var title : String
+    var content : String
+    var date : Date
+}
---
+++ Notas/Model/NotesManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotesManager.swift
+//  Notas
+//
+//  Created by Hugo Adrián Meza Vega on 19/04/24.
+//
+
+import Foundation
